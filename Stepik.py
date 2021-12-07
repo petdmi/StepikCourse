09579c8612ac27e43bd45161f72dc93a25fe867e
Dmitry Petrov <petdmi@gmail.com>
2021-12-07 22:06:13 +0800

    [win] alishev цикл while *недосмотренно занятие

--- Stepik.py
+++ Stepik.py
@@ -144,4 +144,37 @@
 # print(x)
 #######################################################################
 
+# # #Alishev ЦИКЛЫ WHILE
+# total = 0
+# for i in range (1,5):
+#     total += i
+# print(total)
+#
+# total2 = 0
+# i1 = 0
+# while i1 < 5:
+#     total2+=i1
+#     i1+=1
 
+my_list = [7,5,4,3,2,1,-5,-10,-15]
+total3 = 0
+i3 = 0
+while my_list[i3] > 0:
+    total3+=my_list[i3]
+    i3+=1
+
+
+total4 = 0
+for element in my_list:
+    if element > 0:
+        total4+=element
+
+print(total3)
+print(total4)
+
+total5 = 0
+i5 = 0
+while total5 < 10 and my_list[i5] > 0:
+    total5+=my_list[i5]
+    i5+=1
+print(total5)
